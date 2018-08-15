adc06aaab2cf6971d88aadfe179709aaa410b4b7
Zac Bell <zchrbell7@gmail.com>
2018-08-15 16:23:18 -0600

    v3

--- Lookup-Time-Comparison/settings.py
+++ Lookup-Time-Comparison/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Lookup-Time-Comparison.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates', 'static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
