2863a8768b62ffacfe0b4f85fcdb3541c6350fcb
Zac Bell <zchrbell7@gmail.com>
2018-08-25 15:50:29 -0600

    formatting

--- LookupTimes.py
+++ LookupTimes.py
@@ -2,14 +2,17 @@ import csv
 import itertools
 import time
 
-start_time = time.time()
-
 app_set = set(())
 app_list = []
 app_dictionary = {}
 
+print("\n---------------------------------------------------")
+print("\033[94mCoding Challenge:\033[0m\nCompare lookup times for set, list, and dictionary ")
+print("---------------------------------------------------")
+
+start_time = time.time()
 with open('AppleStore.csv') as csv_file:
-    for row in itertools.islice(csv.DictReader(csv_file), 10000):
+    for row in itertools.islice(csv.DictReader(csv_file), 11000):
         app_set.add(row['track_name'])
         app_list.append(row['track_name'])
         app_dictionary[row['size_bytes']] = row['track_name']
@@ -17,42 +20,39 @@ with open('AppleStore.csv') as csv_file:
 # print("Set data: ", app_set)
 # print("List data: ", app_list)
 # print("Dictionary data: ", app_dictionary)
-print("\nTime to initialize 10,000 rows into a set,list, and dictionary: {:.10f}s".format(time.time() - start_time))
-
+print("\nElapsed time to initialize structures \nwith 11,000 rows of data:\n\033[92m{:.10f}s\033[0m".format(time.time() - start_time))
 
 # ASK FOR USER INPUT
-n = str(input("\nEnter an App Name to search from the set, list, and dictionary: "))
-
+n = str(input("\nSearch for an app by name: "))
 
 # -----------------------SET LOOKUP----------------------------------
 app_set_start_time = time.time()
 if n in app_set:
-    print("\nMatch found in set")
+    print("\nSet:                    \033[92mMatch found\033[0m")
 else:
-    print("\nMatch not found in set")
+    print("\nSet:                    \033[92mMatch not found\033[0m")
 set_time = time.time() - app_set_start_time
-
 
 # -----------------------LIST LOOKUP---------------------------------
 app_list_start_time = time.time()
 if n in app_list:
-    print("Match found in list")
+    print("List:                   \033[92mMatch found\033[0m")
 else:
-    print("Match not found in list")
+    print("List:                   \033[92mMatch not found\033[0m")
 list_time = time.time() - app_list_start_time
-
 
 # -----------------------DICTIONARY LOOKUP---------------------------
 app_dictionary_start_time = time.time()
 if n in app_dictionary.values():
-    print("Match found in dictionary")
+    print("Dictionary:             \033[92mMatch found\033[0m")
 else:
-    print("Match not found in dictionary")
+    print("Dictionary:             \033[92mMatch not found\033[0m")
 dict_time = time.time() - app_dictionary_start_time
 
-print("\nSet lookup time: {:.10f}s".format(set_time))
-print("List lookup time: {:.10f}s".format(list_time))
-print("Dictionary lookup time: {:.10f}s".format(dict_time))
+# PRINT RESULTS
+print("\nSet Lookup Time:        \033[92m{:.10f}s\033[0m".format(set_time))
+print("List Lookup Time:       \033[92m{:.10f}s\033[0m".format(list_time))
+print("Dictionary Lookup Time: \033[92m{:.10f}s\033[0m".format(dict_time))
 
-print("\nSLOWEST: {:.10f}s".format(max(set_time, list_time, dict_time)))
-print("FASTEST: {:.10f}s".format(min(set_time, list_time, dict_time)))
+print("\nSlowest Time:           \033[92m{:.10f}s\033[0m".format(max(set_time, list_time, dict_time)))
+print("Fastest Time:           \033[92m{:.10f}s\033[0m".format(min(set_time, list_time, dict_time)))
